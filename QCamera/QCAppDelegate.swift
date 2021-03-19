40dd112e9953124436320e6ed894a78f84f0f4ce
alessaba <alessandrosaba023@gmail.com>
2021-03-19 10:41:15 +0100

    Semicolons aren't needed

--- QCamera/QCAppDelegate.swift
+++ QCamera/QCAppDelegate.swift
@@ -32,41 +32,41 @@ class QCAppDelegate: NSObject, NSApplicationDelegate, NSWindowDelegate, QCUsbWat
 	var isAspectRatioFixed: Bool = UserDefaults.standard.bool(forKey: "isAspectRatioFixed")
     
     // 0 = normal, 1 = 90' top to right, 2 = 180' top to bottom, 3 = 270' top to left
-	var position = UserDefaults.standard.integer(forKey: "position");
+	var position = UserDefaults.standard.integer(forKey: "position")
 	
-    var defaultBorderStyle: NSWindow.StyleMask = NSWindow.StyleMask.closable;
-    var windowTitle = "Quick Camera";
-    let defaultDeviceIndex: Int = 0;
+    var defaultBorderStyle: NSWindow.StyleMask = NSWindow.StyleMask.closable
+    var windowTitle = "Quick Camera"
+    let defaultDeviceIndex: Int = 0
     var selectedDeviceIndex: Int = 0
     
-    var devices: [AVCaptureDevice]!;
-    var captureSession: AVCaptureSession!;
-    var captureLayer: AVCaptureVideoPreviewLayer!;
-	var cachedSavePath: URL? = nil;
+    var devices: [AVCaptureDevice]!
+    var captureSession: AVCaptureSession!
+    var captureLayer: AVCaptureVideoPreviewLayer!
+	var cachedSavePath: URL? = nil
     
-    var input: AVCaptureDeviceInput!;
+    var input: AVCaptureDeviceInput!
 
     func errorMessage(message: String){
-        let popup = NSAlert();
-        popup.messageText = message;
-        popup.runModal();
+        let popup = NSAlert()
+        popup.messageText = message
+        popup.runModal()
     }
     
     func detectVideoDevices() {
-        NSLog("Detecting video devices...");
-        self.devices = AVCaptureDevice.devices(for: AVMediaType.video);
+        NSLog("Detecting video devices...")
+        self.devices = AVCaptureDevice.devices(for: AVMediaType.video)
         
         if (devices?.count == 0) {
-            let popup = NSAlert();
-            popup.messageText = "Unfortunately, you don't appear to have any cameras connected. Goodbye for now!";
-            popup.runModal();
-            NSApp.terminate(nil);
+            let popup = NSAlert()
+            popup.messageText = "Unfortunately, you don't appear to have any cameras connected. Goodbye for now!"
+            popup.runModal()
+            NSApp.terminate(nil)
         } else {
-            NSLog("%d devices found", devices?.count ?? 0);
+            NSLog("%d devices found", devices?.count ?? 0)
         }
         
-        let deviceMenu = NSMenu();
-        var deviceIndex = 0;
+        let deviceMenu = NSMenu()
+        var deviceIndex = 0
 
         // Here we need to keep track of the current device (if selected) in order to keep it checked in the menu
         var currentDevice = self.devices[defaultDeviceIndex]
@@ -77,24 +77,24 @@ class QCAppDelegate: NSObject, NSApplicationDelegate, NSWindowDelegate, QCUsbWat
         
         for device in self.devices {
             let deviceMenuItem = NSMenuItem(title: device.localizedName, action: #selector(deviceMenuChanged), keyEquivalent: "")
-            deviceMenuItem.target = self;
-            deviceMenuItem.representedObject = deviceIndex;
+            deviceMenuItem.target = self
+            deviceMenuItem.representedObject = deviceIndex
             if (device == currentDevice) {
-                deviceMenuItem.state = NSControl.StateValue.on;
+                deviceMenuItem.state = NSControl.StateValue.on
                 self.selectedDeviceIndex = deviceIndex
             }
             if (deviceIndex < 9) {
-                deviceMenuItem.keyEquivalent = String(deviceIndex + 1);
+                deviceMenuItem.keyEquivalent = String(deviceIndex + 1)
             }
-            deviceMenu.addItem(deviceMenuItem);
-            deviceIndex += 1;
+            deviceMenu.addItem(deviceMenuItem)
+            deviceIndex += 1
         }
-        selectSourceMenu.submenu = deviceMenu;
+        selectSourceMenu.submenu = deviceMenu
     }
     
     func startCaptureWithVideoDevice(defaultDevice: Int) {
-        NSLog("Starting capture with device index %d", defaultDevice);
-        let device: AVCaptureDevice = self.devices[defaultDevice];
+        NSLog("Starting capture with device index %d", defaultDevice)
+        let device: AVCaptureDevice = self.devices[defaultDevice]
         
         if (captureSession != nil) {
             
@@ -102,152 +102,152 @@ class QCAppDelegate: NSObject, NSApplicationDelegate, NSWindowDelegate, QCUsbWat
             let currentDevice = (self.captureSession.inputs[0] as! AVCaptureDeviceInput).device
             guard currentDevice != device else { return }
             
-            captureSession.stopRunning();
+            captureSession.stopRunning()
         }
-        captureSession = AVCaptureSession();
+        captureSession = AVCaptureSession()
         
         do {
-            self.input = try AVCaptureDeviceInput(device: device);
-            self.captureSession.addInput(input);
-            self.captureSession.startRunning();
-            self.captureLayer = AVCaptureVideoPreviewLayer(session: self.captureSession);
-            self.captureLayer.connection?.automaticallyAdjustsVideoMirroring = false;
-            self.captureLayer.connection?.isVideoMirrored = false;
+            self.input = try AVCaptureDeviceInput(device: device)
+            self.captureSession.addInput(input)
+            self.captureSession.startRunning()
+            self.captureLayer = AVCaptureVideoPreviewLayer(session: self.captureSession)
+            self.captureLayer.connection?.automaticallyAdjustsVideoMirroring = false
+            self.captureLayer.connection?.isVideoMirrored = false
             
-            self.playerView.layer = self.captureLayer;
-            self.playerView.controlsStyle = AVPlayerViewControlsStyle.none;
-            self.playerView.layer?.backgroundColor = CGColor.black;
-            self.windowTitle = String(format: "Quick Camera: [%@]", device.localizedName);
-            self.window.title = self.windowTitle;
-            fixAspectRatio();
+            self.playerView.layer = self.captureLayer
+            self.playerView.controlsStyle = AVPlayerViewControlsStyle.none
+            self.playerView.layer?.backgroundColor = CGColor.black
+            self.windowTitle = String(format: "Quick Camera: [%@]", device.localizedName)
+            self.window.title = self.windowTitle
+            fixAspectRatio()
         } catch {
-            NSLog("Error while opening device");
-            self.errorMessage(message: "Unfortunately, there was an error when trying to access the camera. Try again or select a different one.");
+            NSLog("Error while opening device")
+            self.errorMessage(message: "Unfortunately, there was an error when trying to access the camera. Try again or select a different one.")
         }
     }
     
    
     
     @IBAction func mirrorHorizontally(_ sender: NSMenuItem) {
-        NSLog("Mirror image menu item selected");
+        NSLog("Mirror image menu item selected")
 		isMirrored.toggle()
 		UserDefaults.standard.set(isMirrored, forKey: "isMirrored")
-        self.captureLayer.connection?.isVideoMirrored = isMirrored;
+        self.captureLayer.connection?.isVideoMirrored = isMirrored
     }
     
     func setRotation(_ position: Int){
         switch (position){
         case 1: if (!isUpsideDown){
-            self.captureLayer.connection?.videoOrientation = AVCaptureVideoOrientation.landscapeLeft;
+            self.captureLayer.connection?.videoOrientation = AVCaptureVideoOrientation.landscapeLeft
         } else {
-            self.captureLayer.connection?.videoOrientation = AVCaptureVideoOrientation.landscapeRight;
+            self.captureLayer.connection?.videoOrientation = AVCaptureVideoOrientation.landscapeRight
         }
-        break;
+        break
         case 2: if (!isUpsideDown){
-            self.captureLayer.connection?.videoOrientation = AVCaptureVideoOrientation.portraitUpsideDown;
+            self.captureLayer.connection?.videoOrientation = AVCaptureVideoOrientation.portraitUpsideDown
         } else {
-            self.captureLayer.connection?.videoOrientation = AVCaptureVideoOrientation.portrait;
+            self.captureLayer.connection?.videoOrientation = AVCaptureVideoOrientation.portrait
         }
-        break;
+        break
         case 3: if (!isUpsideDown) {
-            self.captureLayer.connection?.videoOrientation = AVCaptureVideoOrientation.landscapeRight;
+            self.captureLayer.connection?.videoOrientation = AVCaptureVideoOrientation.landscapeRight
         } else {
-            self.captureLayer.connection?.videoOrientation = AVCaptureVideoOrientation.landscapeLeft;
+            self.captureLayer.connection?.videoOrientation = AVCaptureVideoOrientation.landscapeLeft
         }
-        break;
+        break
         case 0: if (!isUpsideDown) {
-            self.captureLayer.connection?.videoOrientation = AVCaptureVideoOrientation.portrait;
+            self.captureLayer.connection?.videoOrientation = AVCaptureVideoOrientation.portrait
         } else {
-            self.captureLayer.connection?.videoOrientation = AVCaptureVideoOrientation.portraitUpsideDown;
+            self.captureLayer.connection?.videoOrientation = AVCaptureVideoOrientation.portraitUpsideDown
         }
-        break;
-        default: break;
+        break
+        default: break
         }
     }
     
     @IBAction func mirrorVertically(_ sender: NSMenuItem) {
-        NSLog("Mirror image vertically menu item selected");
+        NSLog("Mirror image vertically menu item selected")
 		isUpsideDown.toggle()
 		UserDefaults.standard.set(isUpsideDown, forKey: "isUpsideDown")
-        setRotation(position);
+        setRotation(position)
 		isMirrored.toggle()
-        self.captureLayer.connection?.isVideoMirrored = isMirrored;
+        self.captureLayer.connection?.isVideoMirrored = isMirrored
     }
     
     @IBAction func rotateLeft(_ sender: NSMenuItem) {
-        NSLog("Rotate Left menu item selected with position %d", position);
-        position = position - 1;
-        if (position == -1) { position = 3;}
+        NSLog("Rotate Left menu item selected with position %d", position)
+        position = position - 1
+        if (position == -1) { position = 3}
 		UserDefaults.standard.set(position, forKey: "position")
-        setRotation(position);
+        setRotation(position)
     }
     
     @IBAction func rotateRight(_ sender: NSMenuItem) {
-        NSLog("Rotate Right menu item selected with position %d", position);
-        position = position + 1;
-        if (position == 4) { position = 0;}
+        NSLog("Rotate Right menu item selected with position %d", position)
+        position = position + 1
+        if (position == 4) { position = 0}
 		UserDefaults.standard.set(position, forKey: "position")
-        setRotation(position);
+        setRotation(position)
     }
         
     private func addBorder(){
-        window.styleMask = defaultBorderStyle;
-        window.title = self.windowTitle;
-        self.window.level = convertToNSWindowLevel(Int(CGWindowLevelForKey(.normalWindow)));
-        window.isMovableByWindowBackground = false;
+        window.styleMask = defaultBorderStyle
+        window.title = self.windowTitle
+        self.window.level = convertToNSWindowLevel(Int(CGWindowLevelForKey(.normalWindow)))
+        window.isMovableByWindowBackground = false
     }
     
     private func removeBorder() {
-        defaultBorderStyle = window.styleMask;
-        self.window.styleMask = [NSWindow.StyleMask.borderless, NSWindow.StyleMask.resizable];
-        self.window.level = convertToNSWindowLevel(Int(CGWindowLevelForKey(.maximumWindow)));
-        window.isMovableByWindowBackground = true;
+        defaultBorderStyle = window.styleMask
+        self.window.styleMask = [NSWindow.StyleMask.borderless, NSWindow.StyleMask.resizable]
+        self.window.level = convertToNSWindowLevel(Int(CGWindowLevelForKey(.maximumWindow)))
+        window.isMovableByWindowBackground = true
     }
     
     @IBAction func borderless(_ sender: NSMenuItem) {
-        NSLog("Borderless menu item selected");
+        NSLog("Borderless menu item selected")
         if (self.window.styleMask.contains(.fullScreen)){
-            NSLog("Ignoring borderless command as window is full screen");
-            return;
+            NSLog("Ignoring borderless command as window is full screen")
+            return
         }
 		isBorderless.toggle()
 		UserDefaults.standard.set(isBorderless, forKey: "isBorderless")
-        sender.state = convertToNSControlStateValue((isBorderless ? NSControl.StateValue.on.rawValue : NSControl.StateValue.off.rawValue));
+        sender.state = convertToNSControlStateValue((isBorderless ? NSControl.StateValue.on.rawValue : NSControl.StateValue.off.rawValue))
         if (isBorderless) {
             removeBorder()
         } else {
             addBorder()
         }
-        fixAspectRatio();
+        fixAspectRatio()
         
     }
     
     @IBAction func enterFullScreen(_ sender: NSMenuItem) {
-        NSLog("Enter full screen menu item selected");
-        playerView.window?.toggleFullScreen(self);
+        NSLog("Enter full screen menu item selected")
+        playerView.window?.toggleFullScreen(self)
     }
     
     @IBAction func toggleFixAspectRatio(_ sender: NSMenuItem) {
 		isAspectRatioFixed.toggle()
 		UserDefaults.standard.set(isAspectRatioFixed, forKey: "isAspectRatioFixed")
-        sender.state = convertToNSControlStateValue((isAspectRatioFixed ? NSControl.StateValue.on.rawValue : NSControl.StateValue.off.rawValue));
-        fixAspectRatio();
+        sender.state = convertToNSControlStateValue((isAspectRatioFixed ? NSControl.StateValue.on.rawValue : NSControl.StateValue.off.rawValue))
+        fixAspectRatio()
     }
     
     func fixAspectRatio() {
         if isAspectRatioFixed, #available(OSX 10.15, *) {
             let height = input.device.activeFormat.formatDescription.dimensions.height
-            let width = input.device.activeFormat.formatDescription.dimensions.width;
-            let size = NSMakeSize(CGFloat(width), CGFloat(height));
-            self.window.contentAspectRatio = size;
+            let width = input.device.activeFormat.formatDescription.dimensions.width
+            let size = NSMakeSize(CGFloat(width), CGFloat(height))
+            self.window.contentAspectRatio = size
             
-            let ratio = CGFloat(Float(width)/Float(height));
+            let ratio = CGFloat(Float(width)/Float(height))
             
-            var currentSize = self.window.contentLayoutRect.size;
-            currentSize.height = currentSize.width / ratio;
-            self.window.setContentSize(currentSize);
+            var currentSize = self.window.contentLayoutRect.size
+            currentSize.height = currentSize.width / ratio
+            self.window.setContentSize(currentSize)
         } else {
-            self.window.contentResizeIncrements = NSMakeSize(1.0,1.0);
+            self.window.contentResizeIncrements = NSMakeSize(1.0,1.0)
         }
     }
 	
@@ -257,8 +257,8 @@ class QCAppDelegate: NSObject, NSApplicationDelegate, NSWindowDelegate, QCUsbWat
 
     @IBAction func saveImage(_ sender: NSMenuItem?) {
         if (self.window.styleMask.contains(.fullScreen)){
-            NSLog("Save is not supported as window is full screen");
-            return;
+            NSLog("Save is not supported as window is full screen")
+            return
         }
         
         if (captureSession != nil){
@@ -266,7 +266,7 @@ class QCAppDelegate: NSObject, NSApplicationDelegate, NSWindowDelegate, QCUsbWat
                 // turn borderless on, capture image, return border to previous state
                 let borderlessState = self.isBorderless	
                 if (borderlessState == false) {
-                    NSLog("Removing border");
+                    NSLog("Removing border")
                     self.removeBorder()
                 }
 
@@ -299,7 +299,7 @@ class QCAppDelegate: NSObject, NSApplicationDelegate, NSWindowDelegate, QCUsbWat
 								let destination = CGImageDestinationCreateWithURL(filepath as CFURL, kUTTypePNG, 1, nil)
 								if (destination == nil)
 								{
-									NSLog("Could not write file - destination returned from CGImageDestinationCreateWithURL was nil");
+									NSLog("Could not write file - destination returned from CGImageDestinationCreateWithURL was nil")
 									self.errorMessage(message: "Unfortunately, the image could not be saved to this location.")
 								} else {
 									self.cachedSavePath = panel.url!
@@ -317,32 +317,32 @@ class QCAppDelegate: NSObject, NSApplicationDelegate, NSWindowDelegate, QCUsbWat
                 }
 				
             } else {
-                let popup = NSAlert();
-                popup.messageText = "Unfortunately, saving images is only supported in Mac OSX 10.12 (Sierra) and higher.";
-                popup.runModal();
+                let popup = NSAlert()
+                popup.messageText = "Unfortunately, saving images is only supported in Mac OSX 10.12 (Sierra) and higher."
+                popup.runModal()
             }
         }
     }
     
     @objc func deviceMenuChanged(_ sender: NSMenuItem) {
-        NSLog("Device Menu changed");
+        NSLog("Device Menu changed")
         if (sender.state == NSControl.StateValue.on) {
             // selected the active device, so nothing to do here
-            return;
+            return
         }
         
         // set the checkbox on the currently selected device
         for menuItem: NSMenuItem in selectSourceMenu.submenu!.items {
-            menuItem.state = NSControl.StateValue.off;
+            menuItem.state = NSControl.StateValue.off
         }
-        sender.state = NSControl.StateValue.on;
+        sender.state = NSControl.StateValue.on
         
         self.startCaptureWithVideoDevice(defaultDevice: sender.representedObject as! Int)
     }
     
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        detectVideoDevices();
-        startCaptureWithVideoDevice(defaultDevice: defaultDeviceIndex);
+        detectVideoDevices()
+        startCaptureWithVideoDevice(defaultDevice: defaultDeviceIndex)
 		window.delegate = self
         usb.delegate = self
 		
@@ -350,15 +350,15 @@ class QCAppDelegate: NSObject, NSApplicationDelegate, NSWindowDelegate, QCUsbWat
 			removeBorder()
 		}
 		
-		self.captureLayer.connection?.isVideoMirrored = isMirrored;
+		self.captureLayer.connection?.isVideoMirrored = isMirrored
 		setRotation(position)
 		
-		aspectRatioItem.state = convertToNSControlStateValue((isAspectRatioFixed ? NSControl.StateValue.on.rawValue : NSControl.StateValue.off.rawValue));
-		borderlessItem.state = convertToNSControlStateValue((isBorderless ? NSControl.StateValue.on.rawValue : NSControl.StateValue.off.rawValue));
+		aspectRatioItem.state = convertToNSControlStateValue((isAspectRatioFixed ? NSControl.StateValue.on.rawValue : NSControl.StateValue.off.rawValue))
+		borderlessItem.state = convertToNSControlStateValue((isBorderless ? NSControl.StateValue.on.rawValue : NSControl.StateValue.off.rawValue))
     }
     
     func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
-        true;
+        true
     }
 }
 
